e0eeecffd002066a14e63ef035192ee33d5e54f5
izzy-el <jv.izael@hotmail.com>
2020-02-06 21:33:59 -0300

    Trying to display data in a HTML table using NodeJS.

--- Scripts/dbConnect.js
+++ Scripts/dbConnect.js
@@ -1,52 +1,58 @@
-const { Connection, Request } = require("tedious");
+const http = require('http');
+const mysql = require('mysql');
 
 // Create connection to database
-const config = {
-    authentication: {
-        options: {
-            userName: "frc6217", // update me
-            password: "Cfbombers6217" // update me
-        },
-        type: "default"
-    },
-    server: "team6217.database.windows.net", // update me
-    options: {
-        database: "ScoutApp", //update me
-        encrypt: true
-    }
-};
-
-const connection = new Connection(config);
-
-// Attempt to connect and execute queries if connection goes through
-connection.on("connect", err => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        queryDatabase();
-    }
+const pool = mysql.createPool({
+    host: 'team6217.database.windows.net',
+    user: 'frc6217',
+    password: 'Cfbombers6217',
+    database: 'ScoutApp',
 });
 
-function queryDatabase() {
-    console.log("Reading rows from the Table...");
+var reo = '<html><head><title>Node.js MySQL Select</title></head><body><h1>Node.js MySQL Select</h1>{${table}}</body></html>';
 
-    // Read all rows from table
-    const request = new Request(
-        'SELECT * from Match',
-        (err, rowCount) => {
-            if (err) {
-                console.error(err.message);
-            } else {
-                console.log(`${rowCount} row(s) returned`);
-            }
+function setResHtml(sql, cb) {
+    pool.getConnection((err, con) => {
+        if(err) {
+            console.error(err.message);
+            console.log("Here One");
         }
-    );
+        else {
+            con.query(sql, (err, res, cols) => {
+                if (err) {
+                    console.error(err.message);
+                    console.log("Here Two");
+                }
+                else {
+                    var table = '';
 
-    request.on("row", columns => {
-        columns.forEach(column => {
-            console.log("%s\t%s", column.metadata.colName, column.value);
-        });
+                    for (var i = 0; i < res.length; i++) {
+                        table += '<tr><td>' + res[i].matchReportUrl + '</td><td>' + res[i].r1TeamNumber + '</td><td>' + res[i].r1TeamReportUrl + '</td><td>' + res[i].r1TeamScoutUrl + '</td><td>' + res[i].r2TeamNumber + '</td><td>' + res[i].r2TeamReportUrl + '</td><td>' + res[i].r2TeamScoutUrl + '</td><td>' + res[i].r3TeamNumber + '</td><td>' + res[i].r3TeamReportUrl + '</td><td>' + res[i].r3TeamScoutUrl + '</td><td>' + res[i].b1TeamNumber + '</td><td>' + res[i].b1TeamReportUrl + '</td><td>' + res[i].b1TeamAcoutUrl + '</td><td>' + res[i].b2TeamNumber + '</td><td>' + res[i].b2TeamScoutUrl + '</td><td>' + res[i].b3TeamNumber + '</td><td>' + res[i].b3TeamReportUrl + '</td><td>' + res[i].b3TeamScoutUrl + '</td></tr>';
+                    }
+                    table = '<table border="1"><tr><th>Match </th><th>Time</th><th>Red 1</th><th>S</th><th>Red 2</th><th>S</th><th>Red 3</th><th>S</th><th>Blu 1</th><th>S</th><th>Blu 2</th><th>S</th><th>Blu 3</th><th>S</th><th>Red Sc</th><th>Blu Sc</th></tr>' + table + '</table>';
+
+                    con.release();
+
+                    return cb(table);
+                }
+            });
+        }  
     });
-
-    connection.execSql(request);
 }
+
+let sql = 'select matchReportUrl, r1TeamNumber, r1TeamReportUrl, r1TeamScoutUrl, r2TeamNumber, r2TeamReportUrl, r2TeamScoutUrl, r3TeamNumber, r3TeamReportUrl, r3TeamScoutUrl, b1TeamNumber, b1TeamReportUrl, b1TeamScoutUrl, b2TeamNumber, b2TeamReportUrl, b2TeamScoutUrl, b3TeamNumber, b3TeamReportUrl, b3TeamScoutUrl, sortOrder, matchNumber, matchId, datetime, redScore, blueScore, r1TeamId, r2TeamId, r3TeamId, b1TeamId, b2TeamId, b3TeamId from v_MatchHyperlinks order by sortOrder, datetime, matchNumber';
+/*
+//create the server for browser access
+const server = http.createServer((req, res) => {
+    setResHtml(sql, resql => {
+        reo = reo.replace('{${table}}', resql);
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.write(reo, 'utf-8');
+        res.end();
+    });
+});
+
+server.listen(8080, () => {
+    console.log('Server running at //localhost:8080/');
+});
+*/
